718eec1c990f6f442fa5395e673c2472d3a8d96e
Farnoosh Tavakkoli <attarfarnoosh@gmail.com>
2023-08-08 04:11:38 -0400

    redone

---
+++ lib/migrations/versions/14164135aab0_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 14164135aab0
+Revises: 
+Create Date: 2023-08-08 04:04:17.451960
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '14164135aab0'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
